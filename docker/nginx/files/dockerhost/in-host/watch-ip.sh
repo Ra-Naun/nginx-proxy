0ec16ab37b3c4c644292474d29b3012e8b3e749f
Ra-Naun <dmitriy_slavskiy@mail.ru>
2023-03-25 11:36:10 +0100

    WI-4 | refactoring

--- docker/nginx/files/dockerhost/in-host/watch-ip.sh
+++ docker/nginx/files/dockerhost/in-host/watch-ip.sh
@@ -4,7 +4,7 @@
 
 ATTEMPTS_5s=1000;
 ATTEMPTS_10s=1000;
-ATTEMPTS_60s=1000;
+ATTEMPTS_30s=1000;
 
 while true;
 do
@@ -14,10 +14,10 @@ do
     elif [ $ATTEMPTS_10s -gt 0 ]; then
         sleep 10;
         ATTEMPTS_10s=$((ATTEMPTS_10s-1))
-    elif [ $ATTEMPTS_60s -gt 0 ]; then
-        sleep 60;
-        ATTEMPTS_60s=$((ATTEMPTS_60s-1))
-    else sleep 600;
+    elif [ $ATTEMPTS_30s -gt 0 ]; then
+        sleep 30;
+        ATTEMPTS_30s=$((ATTEMPTS_30s-1))
+    else break;
     fi
 
     ./docker/nginx/files/dockerhost/in-host/get-ip.sh; 
